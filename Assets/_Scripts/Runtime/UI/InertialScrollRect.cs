9c9b1816bce6b90d64d40dc591071fa7f12d4bea
Gunaraj Poojary <gunarajpoojary579@gmail.com>
2025-05-17 12:26:27 +0530

    Refactored

---
+++ Assets/_Scripts/Runtime/UI/InertialScrollRect.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Realfy
+{
+    public class InertialScrollRect : ScrollRect
+    {
+        private bool _draggingByTouch;
+        private bool _isDragging;
+        private Vector2 _prevPosition = Vector2.zero;
+        //[SerializeField] private TMP_InputField ealsticityText;
+        //[SerializeField] private TMP_InputField decelerationRateText;
+        //[SerializeField] private TMP_InputField scrollSensitivityText;
+
+        //protected override void OnEnable()
+        //{
+        //    base.OnEnable();
+
+        //    ealsticityText.onEndEdit.AddListener((Value) => elasticity = float.Parse(Value));
+        //    decelerationRateText.onEndEdit.AddListener((Value) => decelerationRate = float.Parse(Value));
+        //    scrollSensitivityText.onEndEdit.AddListener((Value) => scrollSensitivity = float.Parse(Value));
+        //}
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            StopAllCoroutines();
+        }
+
+        public override void OnBeginDrag(PointerEventData eventData)
+        {
+            base.OnBeginDrag(eventData);
+
+            _draggingByTouch = eventData.pointerId != -1;
+            _isDragging = true;
+        }
+
+        public override void OnEndDrag(PointerEventData eventData)
+        {
+            base.OnEndDrag(eventData);
+
+            _isDragging = false;
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            _isDragging = false;
+        }
+
+        public override void Rebuild(CanvasUpdate executing)
+        {
+            base.Rebuild(executing);
+
+            if (executing == CanvasUpdate.PostLayout)
+            {
+                _prevPosition = content.anchoredPosition;
+            }
+        }
+
+        protected override void LateUpdate()
+        {
+            base.LateUpdate();
+
+            var deltaTime = Time.unscaledDeltaTime;
+            if (deltaTime > 0.0f && _isDragging && inertia)
+            {
+                Vector3 newVelocity = (content.anchoredPosition - _prevPosition) / deltaTime;
+                velocity = _draggingByTouch ? newVelocity : Vector3.Lerp(velocity, newVelocity, deltaTime * 10f);
+            }
+
+            _prevPosition = content.anchoredPosition;
+        }
+    }
+}
